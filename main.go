c7c82ffc1f8a51c780a5784bb2135e786d6a8070
Mikewong3 <jwong3398@email.com>
2022-06-28 16:24:10 -0400

    test

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,10 +23,24 @@ type GithubPusher struct {
 	Name string `json:"name"`
 }
 
-type GithubEvent struct {
-	Repository GithubRepository `json:"repository"`
-	Sender     GithubSender     `json:"sender"`
-	Pusher     GithubPusher     `json:"pusher"`
+type GithubWorkflowJob struct {
+	Conclusion string `json:"conclusion"`
+}
+
+type GithubWebhookEvent struct {
+	Repository GithubRepository  `json:"repository"`
+	Sender     GithubSender      `json:"sender"`
+	Pusher     GithubPusher      `json:"pusher"`
+	Action     string            `json:"action"`
+	Workflow   GithubWorkflowJob `json:"workflow_job"`
+}
+
+type GithubEvents struct {
+	Repository     string `json:"repository"`
+	Action         string `json:"action"`
+	Sender         string `json:"user"`
+	Avatar         string `json:"avatar"`
+	WorkflowStatus string `json:"workflow_status"`
 }
 
 func main() {
@@ -32,12 +48,44 @@ func main() {
 	api := Router.Group("/data")
 	{
 		api.POST("/events", persistEvents)
+		api.GET("/events", getEvents)
 	}
 	Router.Run(":8080")
 }
 
+func getEvents(ctx *gin.Context) {
+	file, err := os.Open("data.csv")
+
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+
+	var res []GithubEvents
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		entry := strings.Split(t, ",")
+
+		res = append(res, GithubEvents{
+			Repository:     entry[0],
+			Action:         entry[1],
+			Sender:         entry[2],
+			Avatar:         entry[3],
+			WorkflowStatus: entry[4],
+		})
+
+		ctx.JSON(200, res)
+	}
+}
+
 func persistEvents(ctx *gin.Context) {
-	var event GithubEvent
+	var event GithubWebhookEvent
 
 	if err := ctx.BindJSON(&event); err != nil {
 		return
@@ -54,12 +102,22 @@ func persistEvents(ctx *gin.Context) {
 	}
 
 	var entry string
+
 	entry += ctx.GetHeader("X-GitHub-Event") + ","
 	entry += event.Repository.Name + ","
 	entry += event.Pusher.Name + ","
-	entry += event.Sender.AvatarUrl
+	entry += event.Sender.AvatarUrl + ","
+	entry += event.Workflow.Conclusion
 
 	f.WriteString(entry + "\n")
 
 	ctx.JSON(200, event)
+}
+
+func filterWorkflowJobs(event GithubWebhookEvent) bool {
+	if event.Action != "completed" {
+		return false
+	}
+
+	return true
 }
